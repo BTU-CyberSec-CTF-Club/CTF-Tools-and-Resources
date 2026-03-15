ec114b1882bb4b718ea3f508db1327c5826dd05c
Fabian Mildner <mildnfab@b-tu.de>
2026-03-15 19:48:07 +0100

    Improve pwn template

--- Binary REV & PWN/pwn_template.py
+++ Binary REV & PWN/pwn_template.py
@@ -1,30 +1,42 @@
 from pwn import *
 
 
-def send_print(val):
+def send_print(val, raw=False):
     """
     Sends the given value / string to the target (and prints the data to your console)
 
     Args:
         val: The value or string to send
+        raw: If True, data will be sent as is (as bytes). Otherwise, it is assumed to be a
+             string that will be encoded.
     """
-    target.send(str(val).encode())
-    print(val, end="")
+    if raw:
+        target.send(val)
+        print(val.decode("utf-8", errors="replace"), end="")
+    else:
+        target.send(str(val).encode())
+        print(val, end="")
 
 
-def sendline_print(val):
+def sendline_print(val, raw=False):
     """
     Sends the given value / string to the target and finishes the current line (and prints
     the data to your console)
 
     Args:
         val: The value or string to send
+        raw: If True, data will be sent as is (should be given as bytes). Otherwise, it
+             will be taken as a string (or converted to one) and sent to the server that way.
     """
-    target.sendline(str(val).encode())
-    print(val)
+    if raw:
+        target.sendline(val)
+        print(val.decode("utf-8", errors="replace"))
+    else:
+        target.sendline(str(val).encode())
+        print(val)
 
 
-def recvuntil_print(val):
+def recvuntil_print(val, raw=False):
     """
     Receive data from the target until the specified value / string is found in the data
     stream.
@@ -35,10 +47,15 @@ def recvuntil_print(val):
 
     Args:
         val: The value or string to look for
+        raw: If True, data will be sent as is (should be given as bytes). Otherwise, it
+             will be taken as a string (or converted to one) and sent to the server that way.
 
     Returns: The data (string) you received
     """
-    resp = target.recvuntil(val)
+    if raw:
+        resp = target.recvuntil(val)
+    else:
+        resp = target.recvuntil(str(val).encode())
     print(resp.decode(), end="")
     return resp.decode()
 
